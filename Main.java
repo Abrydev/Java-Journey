9ea4ea3220622c6268a526a63a98eddb641b7b70
Aubrey <rmuhammadaubrey@gmail.com>
2026-05-02 22:39:29 +0700

    my first project

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        /* 
+            Multi Line Comment
+        */ 
+       //single line comment
+
+        // MY first code
+        System.out.print("Hello World\n");
+        System.out.println("my name's Aubrey");
+    }
+}
