ff20970add449d03284458c89eb821651f3857ae
Aubrey <rmuhammadaubrey@gmail.com>
2026-05-03 15:03:03 +0700

    Arithmetic Operations

---
+++ Arithmetic.java
@@ -0,0 +1,44 @@
+package Artihmetic;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+    // Arithmetic operations
+
+    int x = 10;
+    int y = 2;
+    // int z;
+
+    // z = x + y; // Addition
+    // z = x - y; // Subtraction
+    // z = x * y; // Multiplication
+    // z = x / y; // Division  
+    // z = x % y; // Modulus
+
+    // System.out.println(z);
+
+    // Augmented assignment operators
+
+    // x += y;
+    // x -= y;
+    // x *= y;
+    // x /= y;
+    // x %= y;
+
+    // System.out.println(x);
+
+    // increment and decrement operators
+
+    int a = 1;
+
+    a++;
+    // a++;
+    a--;
+
+    System.out.println(a);
+
+    // Order of operations (P-E-M-D-A-S)
+    double result =  3 + 4 * (7 - 5) / 2.0;
+
+    System.out.println(result);
+    }
+}
