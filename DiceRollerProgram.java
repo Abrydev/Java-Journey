42884c6a04e376b66f408402cbf3cdaec9bc553a
Aubrey <rmuhammadaubrey@gmail.com>
2026-05-07 19:13:25 +0700

    Dice Roller Program

---
+++ DiceRollerProgram.java
@@ -0,0 +1,113 @@
+package DiceRollerProgram;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DiceRollerProgram {
+    public static void main(String[] args) {
+        // Declare Variables
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numOfDice;
+        int total = 0;
+
+        // Get # of dice from the user
+        System.out.print("enter the # of dice: ");
+        numOfDice = scanner.nextInt();
+
+        // Check if # of dice > 0
+        if(numOfDice > 0){
+            // Roll all of the dice
+            for(int i = 0; i < numOfDice; i++){
+                int roll = random.nextInt(1, 7);
+                // Get the total
+                printDie(roll);
+                System.out.println("you rolled: " + roll);
+                total += roll;
+            }
+
+            System.out.println("Total: " + total);
+
+        }else{
+            System.out.println("# of dice must be greater than 0");
+        }
+        
+
+        scanner.close();
+    }
+    // Display ASCII of dice
+    static void printDie(int roll){
+        String dice1 = """
+         ------------
+        |            |
+        |     ·      |
+        |            |
+         ------------
+        """;
+
+        String dice2 = """
+         ------------
+        |  ·         |
+        |            |
+        |          · |
+         ------------
+        """;
+
+        String dice3 = """
+         ------------
+        |  ·         |
+        |     ·      |
+        |          · |
+         ------------
+        """;
+
+        String dice4 = """
+         ------------
+        |  ·       · |
+        |            |
+        |  ·       · |
+         ------------
+        """;
+
+        String dice5 = """
+         ------------
+        |  ·       · |
+        |      ·     |
+        |  ·       · |
+         ------------
+        """;
+
+        String dice6 = """
+         ------------
+        |  ·       · |
+        |  ·       · |
+        |  ·       · |
+         ------------
+        """;
+
+        switch (roll) {
+            case 1:
+                System.out.print(dice1);
+                break;
+            case 2:
+                System.out.print(dice2);
+                break;
+            case 3:
+                System.out.print(dice3);
+                break;
+            case 4:
+                System.out.print(dice4);
+                break;
+            case 5:
+                System.out.print(dice5);
+                break;
+            case 6:
+                System.out.print(dice6);
+                break;
+    
+            default:
+                System.out.println("Invalid Roll");
+                break;
+        }
+    }
+}
